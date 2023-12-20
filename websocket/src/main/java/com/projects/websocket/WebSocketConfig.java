b2172efd0a9980dfdb8b6cc67cd56bd3953949ca
ysbc1247 <ystc1247@khu.ac.kr>
2023-12-21 00:02:06 +0900

    [add] websocket config
    handle registry endpoints
    initialize websockethandler bean

--- websocket/src/main/java/com/projects/websocket/WebSocketConfig.java
+++ websocket/src/main/java/com/projects/websocket/WebSocketConfig.java
@@ -1,2 +1,22 @@
-package com.projects.websocket;public class WebSocketConfig {
+package com.projects.websocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler(), "/websocket")
+                .setAllowedOrigins("*");
+
+    }
+
+    @Bean
+    org.springframework.web.socket.WebSocketHandler webSocketHandler() {
+        return new WebSocketHandler();
+    }
 }
